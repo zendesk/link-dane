41c569b7d854f12464c56a63f56a93446152290c
Cherub Kumar <ckumar@zendesk.com>
2016-02-12 20:54:03 -0800

    Flag accepted matches for comparison

--- data/parseHours.rb
+++ data/parseHours.rb
@@ -21,12 +21,13 @@ FLAG = {missing: "NO_DATA", match: "", no_match: "NOT_PARSEABLE", twenty_four: "
 
 def parseable?(hours)
   hours.split(/;/).each do |h|
+    matches = []
     FINAL.match(h.strip) do |m|
-        # puts m.inspect
-        return true
+      matches << m.string
     end
-    # puts "UNABLE TO PARSE:: #{h}"
-    return false
+    return false if matches.empty?
+
+    return matches
   end
 end
 
@@ -42,8 +43,8 @@ def check_hours(hours)
     return FLAG[:missing]
   elsif is_24hours?(hours)
     return FLAG[:twenty_four]
-  elsif parseable?(hours)
-    return FLAG[:match]
+  elsif matched = parseable?(hours)
+    return matched.inspect
   else
     return FLAG[:no_match]
   end
@@ -51,57 +52,56 @@ end
 
 
 def mark_unparseable_hours(*args)
+  filename = 'toJsonJan24.csv'
 
+  # Load the original CSV file
+  ph_up, bh_up, bh_missing, ph_missing = 0, 0, 0, 0
+  rows = CSV.read(filename, headers: true, encoding: "ISO-8859-1", return_headers: false).collect do |row|
+    hash = row.to_hash
 
-    filename = 'toJsonJan24.csv'
+    building_hours = hash["BUILDING HOURS"]
+    program_hours = hash["PROGRAM HOURS"]
+    
+    # Assign a flag for building and program hours
+    bh_flag = check_hours(building_hours)
+    ph_flag = check_hours(program_hours)
 
-    # Load the original CSV file
-    ph_up, bh_up, bh_missing, ph_missing = 0, 0, 0, 0
-    rows = CSV.read(filename, headers: true, encoding: "ISO-8859-1", return_headers: false).collect do |row|
-      hash = row.to_hash
-
-      building_hours = hash["BUILDING HOURS"]
-      program_hours = hash["PROGRAM HOURS"]
-      # Assign a flag for building and program hours
-      bh_flag = check_hours(building_hours)
-      ph_flag = check_hours(program_hours)
-
-      
-      # Calculate statistics
-      if bh_flag == FLAG[:no_match]
-        bh_up = bh_up + 1
-        # puts "BH: #{building_hours}"
-      elsif bh_flag == FLAG[:missing]
-        bh_missing = bh_missing + 1
-      end
-      if ph_flag == FLAG[:no_match]
-        ph_up = ph_up + 1
-        # puts "PH: #{program_hours}"
-      elsif ph_flag == FLAG[:missing]
-        ph_missing = ph_missing + 1
-      end
-
-      # Merge additional data as a hash.
-      hash.merge('Building Hours FLAG' => bh_flag, 'Program Hours FLAG' => ph_flag)
+    
+    # Calculate statistics
+    if bh_flag == FLAG[:no_match]
+      bh_up = bh_up + 1
+      # puts "BH: #{building_hours}"
+    elsif bh_flag == FLAG[:missing]
+      bh_missing = bh_missing + 1
     end
-    puts "PH unparseable: #{ph_up}"
-    puts "BH unparseable: #{bh_up}"
-    puts "BH missing: #{bh_missing}"
-    puts "PH missing: #{ph_missing}"
-    puts "Total rows: #{rows.count}"
-
-    # # Extract column names from first row of data
-    column_names = rows.first.keys
-    txt = CSV.generate do |csv|
-      csv << column_names
-      rows.each do |row|
-        # Extract values for row of data
-        csv << row.values
-      end
+    if ph_flag == FLAG[:no_match]
+      ph_up = ph_up + 1
+      # puts "PH: #{program_hours}"
+    elsif ph_flag == FLAG[:missing]
+      ph_missing = ph_missing + 1
     end
 
-    # Write to new csv file
-    File.open('marked_hours.csv', 'w') { |file| file.write(txt) }
+    # Merge additional data as a hash.
+    hash.merge('Building Hours FLAG' => bh_flag, 'Program Hours FLAG' => ph_flag)
+  end
+  puts "PH unparseable: #{ph_up}"
+  puts "BH unparseable: #{bh_up}"
+  puts "BH missing: #{bh_missing}"
+  puts "PH missing: #{ph_missing}"
+  puts "Total rows: #{rows.count}"
+
+  # # Extract column names from first row of data
+  column_names = rows.first.keys
+  txt = CSV.generate do |csv|
+    csv << column_names
+    rows.each do |row|
+      # Extract values for row of data
+      csv << row.values
+    end
+  end
+
+  # Write to new csv file
+  File.open('marked_hours.csv', 'w') { |file| file.write(txt) }
         
 end
 
