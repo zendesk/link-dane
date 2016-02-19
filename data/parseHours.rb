92d78e1d87b0331d067f1d4b18c7f2ad3f5b4c7d
Cherub Kumar <ckumar@zendesk.com>
2016-02-19 12:06:58 -0800

    Set-up initial conversion algorithm

--- data/parseHours.rb
+++ data/parseHours.rb
@@ -2,22 +2,50 @@ require 'csv'
 require 'byebug'
 
 
-MONDAY = /Mo(n(day)?)?/
-TUESDAY = /Tu(e(sday)?)?/
-WEDNESDAY = /We(d(nesday)?)?/
-THURSDAY = /Th(u(rsday)?)?/
-FRIDAY = /Fr(i(day)?)?/
-SATURDAY = /Sa(t(urday)?)?/
-SUNDAY = /Su(n(day)?)?/
+MONDAY = /Mo(n(day(s)?)?)?/
+TUESDAY = /Tu(e(sday(s)?)?)?/
+WEDNESDAY = /We(d(nesday(s)?)?)?/
+THURSDAY = /Th(u(rsday(s)?)?)?/
+FRIDAY = /Fr(i(day(s)?)?)?/
+SATURDAY = /Sa(t(urday(s)?)?)?/
+SUNDAY = /Su(n(day(s)?)?)?/
 WEEKDAY = /#{MONDAY}|#{TUESDAY}|#{WEDNESDAY}|#{THURSDAY}|#{FRIDAY}|#{SATURDAY}|#{SUNDAY}/
 # WEEKDAY = /(Mo(n(day)?)?|Tu(e(sday)?)?|We(d(nesday)?)?|Th(u(rsday)?)?|Fr(i(day)?)?|Sa(t(urday)?)?|Su(n(day)?)?)/ 
 WEEKDAY_RANGE = /(?<start_day>#{WEEKDAY})\s*-\s*(?<end_day>#{WEEKDAY})/
 WEEKDAY_LIST = /#{WEEKDAY}(, #{WEEKDAY})*(,? and #{WEEKDAY})?/
-TIME_R = /\d?\d:\d\d[ap]m/
+TIME_R = /\d?\d:\d\d\s*[ap]m/i
 TIME_RANGE_R =  /(?<start_time>#{TIME_R})\s*-\s*(?<end_time>#{TIME_R})/
 FINAL = /(#{WEEKDAY_RANGE}|(?<weekdays>#{WEEKDAY_LIST}|[Dd]aily)),?\s*#{TIME_RANGE_R}/
 
 FLAG = {missing: "NO_DATA", match: "", no_match: "NOT_PARSEABLE", twenty_four: "Confirm 24 hours"  }
+
+def parse_csv_hours
+  filename = 'parse_help.csv'
+
+  # Load the original CSV file
+  rows = CSV.read(filename, headers: true, encoding: "ISO-8859-1", return_headers: false).collect do |row|
+    hash = row.to_hash
+
+    program_hours = hash["Parsed Program Hours"]
+    puts program_hours
+    # parse_hours(program_hours)
+
+  end
+
+end
+
+def parse_hours(hours)
+  parsed = check_hours(hours)
+
+  if parsed == :missing || parsed === :no_match
+    return nil
+  elsif parsed == :twenty_four
+    return 24_hour_json
+  else
+    convert_hours(parsed)
+  end
+    
+end
 
 def parseable?(hours)
   hours.split(/;/).each do |h|
@@ -32,7 +60,7 @@ def parseable?(hours)
 end
 
 def is_24hours?(hours)
-  /24 [hH]ours/.match(hours) do |h|
+  /24\s*[hH]ours/.match(hours) do |h|
     return true
   end
   return false
@@ -40,53 +68,66 @@ end
 
 def check_hours(hours)
   if !hours
-    return FLAG[:missing]
+    return :missing
   elsif is_24hours?(hours)
-    return FLAG[:twenty_four]
+    return :twenty_four
   elsif matched = parseable?(hours)
-    return matched.inspect
+    return matched
   else
-    return FLAG[:no_match]
+    return :no_match
   end
 end
 
+# def check_hours(hours)
+#   if !hours
+#     return FLAG[:missing]
+#   elsif is_24hours?(hours)
+#     return FLAG[:twenty_four]
+#   elsif matched = parseable?(hours)
+#     return matched.inspect
+#   else
+#     return FLAG[:no_match]
+#   end
+# end
+
 
 def mark_unparseable_hours(*args)
-  filename = 'toJsonJan24.csv'
+  filename = 'parse_help.csv'
 
   # Load the original CSV file
   ph_up, bh_up, bh_missing, ph_missing = 0, 0, 0, 0
   rows = CSV.read(filename, headers: true, encoding: "ISO-8859-1", return_headers: false).collect do |row|
     hash = row.to_hash
 
-    building_hours = hash["BUILDING HOURS"]
-    program_hours = hash["PROGRAM HOURS"]
+    # building_hours = hash["BUILDING HOURS"]
+    program_hours = hash["Parsed Program Hours"]
     
     # Assign a flag for building and program hours
-    bh_flag = check_hours(building_hours)
+    # bh_flag = check_hours(building_hours)
     ph_flag = check_hours(program_hours)
 
     
     # Calculate statistics
-    if bh_flag == FLAG[:no_match]
-      bh_up = bh_up + 1
-      # puts "BH: #{building_hours}"
-    elsif bh_flag == FLAG[:missing]
-      bh_missing = bh_missing + 1
-    end
+    # if bh_flag == FLAG[:no_match]
+    #   bh_up = bh_up + 1
+    #   # puts "BH: #{building_hours}"
+    # elsif bh_flag == FLAG[:missing]
+    #   bh_missing = bh_missing + 1
+    # end
     if ph_flag == FLAG[:no_match]
       ph_up = ph_up + 1
-      # puts "PH: #{program_hours}"
+      puts "PH: #{program_hours}"
     elsif ph_flag == FLAG[:missing]
       ph_missing = ph_missing + 1
     end
 
     # Merge additional data as a hash.
-    hash.merge('Building Hours FLAG' => bh_flag, 'Program Hours FLAG' => ph_flag)
+    hash.merge('Program Hours FLAG' => ph_flag)
+    # hash.merge('Building Hours FLAG' => bh_flag, 'Program Hours FLAG' => ph_flag)
   end
   puts "PH unparseable: #{ph_up}"
-  puts "BH unparseable: #{bh_up}"
-  puts "BH missing: #{bh_missing}"
+  # puts "BH unparseable: #{bh_up}"
+  # puts "BH missing: #{bh_missing}"
   puts "PH missing: #{ph_missing}"
   puts "Total rows: #{rows.count}"
 
@@ -105,4 +146,5 @@ def mark_unparseable_hours(*args)
         
 end
 
-mark_unparseable_hours
+parse_csv_hours
+# mark_unparseable_hours
