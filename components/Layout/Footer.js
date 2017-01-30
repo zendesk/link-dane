904df80356dac22835c30f9353f01f90df56b439
Prateek Chhetri <luckyprateek2002@gmail.com>
2017-01-30 03:22:23 -0600

    Update footer :sparkles::sparkles::sparkles:

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -6,12 +6,11 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className={s.footer} role="navigation">
+        <a title="Calls 211 for additional help" href="tel:211">Call 211</a>
         <Link className={s.nav} to="/terms">
           Terms
         </Link>
-        <Link className={s.nav} to="/feedback">
-          Feedback
-        </Link>
+        <a title="Provide feedback about Link-Dane" href="mailto:support@linkdane.zendesk.com">Feedback</a>
         <Link className={s.nav} to="/about">
           About
         </Link>
